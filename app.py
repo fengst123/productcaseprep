68fcedfc3e33d79d801f6d279dbd0abc05b7b0c6
RupasriShankar-s <rupasrishankar@berkeley.edu>
2025-04-20 17:14:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ if st.button("Submit Answer") and user_response:
             })
 
         response = client.chat.completions.create(
-            model="gpt-4",
+            model="gpt-3.5-turbo",
             messages=messages,
             temperature=0.7,
             max_tokens=800
